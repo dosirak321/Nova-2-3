652f2923e1d1b9815454ebb920c26fd7a57801a8
dosirak321 <ryu_dohun0318@naver.com>
2024-10-15 16:10:37 +0900

    Add files via upload

---
+++ do while 응용.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  int sum = 0;
+  do {
+    i = 0;
+    scanf("%d", &i);
+    sum += i;
+  } while (i > 0);
+  printf("%d", sum);
+  printf("%d", count);
+  return 0;
+}
